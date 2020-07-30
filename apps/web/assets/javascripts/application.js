212e7ebd4507c064d847a031ea9414581bcc0db9
Jan Bajena <bajena3@gmail.com>
2020-07-30 20:01:42 +0200

    Add shortcut for adding new words

--- apps/web/assets/javascripts/application.js
+++ apps/web/assets/javascripts/application.js
@@ -13,24 +13,44 @@
     }
   }
 
-  var template = document.getElementById('word-form-template');
-  template.classList.remove('word-form-template');
-  template.remove();
-
-  document.getElementById('add-new-word-button').onclick = function() {
+  function addNewWordForm() {
     var words = document.getElementsByClassName('word-form');
     var newIndex = parseInt(words[words.length - 1].dataset.index || -1) + 1;
 
     var wordFormsDiv = document.getElementById('word-forms');
-    var newTemplate = template.cloneNode(true);
+    var newTemplate = this.template.cloneNode(true);
     newTemplate.dataset.index = newIndex;
     wordFormsDiv.insertAdjacentHTML('beforeend', newTemplate.outerHTML.replace(/words\-0/g, "words-" + newIndex));
     setupRemoveButton(wordFormsDiv.lastChild.querySelector('.remove-word-button'));
   }
 
-  var removeBtns = document.getElementsByClassName('remove-word-button');
+  function setupNewWordTemplate() {
+    this.template = document.getElementById('word-form-template');
+    this.template.classList.remove('word-form-template');
+    this.template.remove();
 
-  for (var i = 0; i < removeBtns.length; i++) {
-    setupRemoveButton(removeBtns[i]);
+    document.getElementById('add-new-word-button').onclick = addNewWordForm;
   }
+
+  function setupRemoveButtons() {
+    var removeBtns = document.getElementsByClassName('remove-word-button');
+
+    for (var i = 0; i < removeBtns.length; i++) {
+      setupRemoveButton(removeBtns[i]);
+    }
+  }
+
+  function setupKeyboardShortcuts() {
+    window.addEventListener('keydown', function(event) {
+      if ((event.metaKey || event.ctrlKey) && event.key === 'Enter') {
+        addNewWordForm();
+        event.stopPropagation();
+        event.preventDefault();
+      }
+    });
+  }
+
+  setupNewWordTemplate();
+  setupRemoveButtons();
+  setupKeyboardShortcuts();
 })();
