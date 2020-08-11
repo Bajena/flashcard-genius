02196039dd42a50b1601cee3ad2318fd36c91c1c
Jan Bajena <bajena3@gmail.com>
2020-08-11 07:45:47 +0200

    Change default route to lists index

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,6 +3,6 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-root to: 'home#index'
+root to: 'word_lists#index'
 resources :word_lists, only: [:new, :create, :show, :edit, :update]
 get '/word_lists', to: 'word_lists#index'
