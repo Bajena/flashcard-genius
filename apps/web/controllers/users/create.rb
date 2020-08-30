29edeef8327d767184b3b0f7489ef7a05f3bd896
Jan Bajena <bajena3@gmail.com>
2020-08-30 14:58:33 +0200

    Add tests for signup

--- apps/web/controllers/users/create.rb
+++ apps/web/controllers/users/create.rb
@@ -8,6 +8,13 @@ module Web
         expose :error_messages
         expose :user
 
+        before do
+          if current_user
+            flash[:error] = "You need to log out in order to create a new account"
+            redirect_to routes.word_lists_path
+          end
+        end
+
         def call(params)
           result = CreateUser.new.call(params[:user])
 
--- spec/web/controllers/users/create_spec.rb
+++ spec/web/controllers/users/create_spec.rb
@@ -1,9 +1,61 @@
 RSpec.describe Web::Controllers::Users::Create, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:response) { action.call(params) }
+  let(:params) { { 'rack.session' => session }.merge(query_params) }
+  let(:query_params) { { user: user_params } }
+  let(:user_params) do
+    {
+      email: email,
+      password: password
+    }
+  end
+  let(:email) { "signup@example.com" }
+  let(:password) { "Test1234" }
+  let(:session) { {} }
+  let!(:user) { create(:user) }
+  let(:exposed_user) { action.exposures[:user] }
+  let(:errors) { action.exposures[:error_messages] }
+  let(:created_user) do
+    UserRepository.
+      new.
+      users.
+      order { created_at.desc }.
+      limit(1).
+      first
+  end
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  context "when user is logged in" do
+    let(:session) { { user_id: user.id } }
+
+    it "redirects to word lists index" do
+      expect do
+        expect(response).to redirect_to("/word_lists")
+      end.not_to change { UserRepository.new.all.count }
+    end
+  end
+
+  context "when signup succeeds" do
+    it "sets current user and redirects" do
+      expect do
+        expect(response).to redirect_to("/word_lists")
+      end.to change { UserRepository.new.all.count }.by(1)
+
+      expect(action.session[:user_id]).to eq(created_user.id)
+      expect(created_user.email).to eq(email)
+    end
+  end
+
+  context "when signup fails" do
+    let(:email) { "invalid" }
+
+    it "renders errors" do
+      expect do
+        expect(response[0]).to eq(422)
+      end.not_to change { UserRepository.new.all.count }
+
+      expect(errors).to eq(["Email is in invalid format"])
+      expect(exposed_user).to be_a(User)
+      expect(exposed_user.email).to eq(nil)
+    end
   end
 end
