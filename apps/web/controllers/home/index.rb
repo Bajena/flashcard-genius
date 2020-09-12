cac5a6b8071435ba58235960893c3e006259c8db
Jan Bajena <bajena3@gmail.com>
2020-09-12 22:12:38 +0200

    Do not show landing page to logged users

--- apps/web/controllers/home/index.rb
+++ apps/web/controllers/home/index.rb
@@ -5,6 +5,10 @@ module Web
         include Web::Action
         include Web::Authentication
 
+        before do
+          redirect_to routes.word_lists_path if current_user
+        end
+
         def call(params)
         end
       end
