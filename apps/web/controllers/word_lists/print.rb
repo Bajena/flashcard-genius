02d12348cddfd9cbed695730dc9c6b28ee7a2809
Jan Bajena <bajena3@gmail.com>
2020-10-11 13:54:13 +0200

    Finalize pdf (#14)
    * Finish PDFs
    
    * Add links

--- apps/web/controllers/word_lists/print.rb
+++ apps/web/controllers/word_lists/print.rb
@@ -7,6 +7,7 @@ module Web
 
         COLUMNS = 2
         ROWS = 3
+        CARD_PADDING = 8
 
         expose :word_list
 
@@ -59,14 +60,14 @@ module Web
             row = page_i / COLUMNS
             col = page_i % COLUMNS
 
-            draw_card(pdf, page, col, row, w.question, w.question_example)
-            draw_card(pdf, page + 1, col, row, w.answer, w.answer_example)
+            draw_card(pdf, page, col, row, w.question, w.question_example, :question)
+            draw_card(pdf, page + 1, col, row, w.answer, w.answer_example, :answer)
           end
 
           @document = pdf.render
         end
 
-        def draw_card(pdf, page, col, row, text, help_text)
+        def draw_card(pdf, page, col, row, text, help_text, type)
           pdf.go_to_page(page)
           height = pdf.bounds.height
           width = pdf.bounds.width
@@ -82,9 +83,64 @@ module Web
           y = row * row_height
 
           pdf.bounding_box([x, top - y], width: card_width, height: card_height) do
-           pdf.text(text)
-           pdf.stroke_bounds
+            print_list_name(pdf)
+            print_main_text(pdf, text, type)
+            print_help_text(pdf, help_text)
+            pdf.stroke_bounds
           end
+        end
+
+        def print_list_name(pdf)
+          y = pdf.bounds.height - CARD_PADDING
+
+          pdf.text_box(
+            word_list.name,
+            at: [CARD_PADDING, y],
+            single_line: true,
+            size: 10,
+            overflow: :shrink_to_fit
+          )
+
+          pdf.dash(2)
+          pdf.stroke_horizontal_line CARD_PADDING, pdf.bounds.width - CARD_PADDING, at: y - 13
+          pdf.undash
+        end
+
+        def print_main_text(pdf, text, type)
+          y = pdf.bounds.height - CARD_PADDING
+          top = y - 25
+          box_size = 16
+          pdf.fill_color(type == :question ? 'D3D3D3' : 'FFFFFF')
+          pdf.fill_and_stroke do
+            pdf.rectangle([CARD_PADDING, top], box_size, box_size)
+          end
+          pdf.fill_color '000000'
+
+          main_text_x = CARD_PADDING + box_size + 5
+          pdf.text_box(
+            text,
+            at: [main_text_x, top - 1],
+            size: 16,
+            height: 16,
+            overflow: :shrink_to_fit,
+            width: pdf.bounds.width - main_text_x - CARD_PADDING
+          )
+        end
+
+        def print_help_text(pdf, text)
+          return unless text
+
+          # PADDING + height of the list name + height of the main text
+          top = pdf.bounds.height - CARD_PADDING - 25 - 1 - 16
+
+          pdf.text_box(text,
+            at: [CARD_PADDING, top],
+            size: 12,
+            align: :center,
+            height: top - CARD_PADDING,
+            width: pdf.bounds.width - 2 * CARD_PADDING,
+            valign: :center
+          )
         end
       end
     end
