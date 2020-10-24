0694891a409ff3dab53a87cf256a870136608548
Jan Bajena <bajena3@gmail.com>
2020-10-24 18:38:10 +0200

    Improve descriptions and add thank you page

---
+++ apps/web/controllers/donate/thank_you.rb
@@ -0,0 +1,13 @@
+module Web
+  module Controllers
+    module Donate
+      class ThankYou
+        include Web::Action
+        include Web::Authentication
+
+        def call(params)
+        end
+      end
+    end
+  end
+end
