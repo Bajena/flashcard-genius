966b6825f93e1750cc5731e1b1e22bb7b616e79a
Jan Bajena <bajena3@gmail.com>
2020-11-14 20:12:06 +0100

    Learn#index specs

--- spec/web/controllers/learn/index_spec.rb
+++ spec/web/controllers/learn/index_spec.rb
@@ -1,9 +1,31 @@
 RSpec.describe Web::Controllers::Learn::Index, type: :action do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) do
+    {
+      'rack.session' => session,
+      'router.params' => router_params
+    }
+  end
+  let(:router_params) { { word_list_id: word_list_id } }
+  let(:response) { action.call(params) }
+  let(:user) { create(:user) }
+  let(:session) { { user_id: user.id } }
+  let(:word_list) do
+    create(:word_list, user_id: user.id)
+  end
+  let(:word_list_id) { word_list.id }
 
-  it 'is successful' do
-    response = action.call(params)
+  context "when user is not logged in" do
+    let(:session) { {} }
+
+    it 'redirects to login' do
+      expect(response).to redirect_to("/login")
+    end
+  end
+
+  it "exposes word_list_id" do
     expect(response[0]).to eq 200
+
+    expect(action.exposures[:word_list_id]).to eq(word_list.id)
   end
 end
