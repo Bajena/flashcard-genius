9f629001877d2d1065a6399be7bf659bb637eebe
Jan Bajena <bajena3@gmail.com>
2020-11-15 13:52:20 +0100

    Session#create specs

--- spec/web/controllers/sessions/create_spec.rb
+++ spec/web/controllers/sessions/create_spec.rb
@@ -1,9 +1,44 @@
 RSpec.describe Web::Controllers::Sessions::Create, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
+  let(:response) { action.call(params) }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  context "when login succceeds" do
+    let(:user) do
+      create(:user, :with_password, email: "user@test.com", password: "Test1234")
+    end
+    let(:params) do
+      {
+        user: {
+          email: user.email,
+          password: "Test1234"
+        }
+      }
+    end
+
+    it "sets session.user_id to user's id" do
+      response
+    end
+
+    it "redirects to word lists" do
+      expect(response).to redirect_to("/word_lists")
+      expect(action.exposures[:flash][:success]).to eq("Logged in succesfully")
+    end
+  end
+
+  context "when login fails" do
+    let(:params) do
+      {
+        user: {
+          email: "xyz@xyz.com",
+          password: "zyx"
+        }
+      }
+    end
+
+    it "redirects to login" do
+      expect(response).to redirect_to("/login")
+      expect(action.exposures[:flash][:error]).to eq("Invalid login information")
+    end
   end
 end
