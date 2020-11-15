1d6a2114b8f1399a18027d91a5ee3f6cd7a0a28e
Jan Bajena <bajena3@gmail.com>
2020-11-15 14:19:30 +0100

    Users#new specs

--- spec/web/controllers/users/new_spec.rb
+++ spec/web/controllers/users/new_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe Web::Controllers::Users::New, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
+  let(:response) { action.call(params) }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  it 'renders the view' do
+    expect(response[0]).to eq(200)
+    expect(action.exposures[:user]).to eq(User.new)
+    expect(action.exposures[:error_messages]).to eq([])
   end
 end
